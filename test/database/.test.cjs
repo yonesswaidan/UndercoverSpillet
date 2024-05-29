413ec98d4eeb1b6b0f653f091ade24ab50e64920
Swaiidan <yonswa01@easv365.dk>
2024-05-29 23:51:13 +0200

    Nyt

--- test/database/.test.cjs
+++ test/database/.test.cjs
@@ -1,8 +1,12 @@
 const mongoose = require('mongoose');
 const { expect } = require('chai');
 
-describe('Database Connection', () => {
+describe('Database Connection', function() {
+  // Forøg timeout til 10 sekunder
+  this.timeout(10000);
+
   before((done) => {
+    mongoose.set('strictQuery', false);  // Håndter strictQuery advarsel
     mongoose.connect('mongodb+srv://yonesswaidan:sgg59mcd@game-database.x2asuuz.mongodb.net/game-database', { useNewUrlParser: true, useUnifiedTopology: true });
     const db = mongoose.connection;
     db.on('error', done);
