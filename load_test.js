f4851b8ad5b6892cf5cb222519b92f59257e0deb
Swaiidan <yonswa01@easv365.dk>
2024-06-16 02:04:49 +0200

    Nyt

--- load_test.js
+++ load_test.js
@@ -1,37 +1,17 @@
 import http from 'k6/http';
-import { sleep, check } from 'k6';
-import { randomItem } from 'https://jslib.k6.io/k6-utils/1.1.0/index.js';
 
-const baseUrl = 'http://192.168.87.167:3000/api/users'; // Erstat 'your-api-url' med det rigtige domænenavn eller IP-adresse
-
-const playerNames = ['John Doe', 'Jane Smith', 'Michael Brown', 'Emily Johnson', 'David Davis'];
-
-export const options = {
+export let options = {
     stages: [
-        { duration: '30s', target: 5 }, // Ramp-up to 25 users over 30 seconds
-        { duration: '30s', target: 5 }, // Stay at 25 users for 30 seconds
-        { duration: '30s', target: 0 },  // Ramp-down to 0 users over 30 seconds
+        { duration: '30s', target: 100 }, 
+        { duration: '50s', target: 100 }, 
+        { duration: '30s', target: 0 }, 
     ],
     thresholds: {
-        http_req_duration: ['p(95)<200'], // 95% of requests should complete below 200ms
-        http_req_failed: ['rate<0.01'],   // Less than 1% of requests should fail
+        http_req_duration: ['p(99)<1500'], // 99% of requests must complete below 1.5s
     },
 };
 
 export default function () {
-    const playerName = randomItem(playerNames);
-    const payload = JSON.stringify({ playerName });
-
-    const params = {
-        headers: {
-            'Content-Type': 'application/json',
-        },
-    };
-
-    const res = http.post(baseUrl, payload, params);
-    check(res, {
-        'is status 201': (r) => r.status === 201,
-    });
-
-    sleep(1);
+    http.get('http://192.168.87.167:3000/api/Users');
 }
+
