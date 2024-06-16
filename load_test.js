1e255b87421a534e867354f5bcf92e876a1fac18
Swaiidan <yonswa01@easv365.dk>
2024-06-16 04:32:02 +0200

    Nyt

--- load_test.js
+++ load_test.js
@@ -1,16 +1,19 @@
 import http from 'k6/http';
+import { check, sleep } from 'k6';
 
 export let options = {
     stages: [
-        { duration: '10s', target: 100 }, // Ramp up to 100 users over 30 seconds
-        { duration: '20s', target: 100 }, // Stay at 100 users for 50 seconds
-        { duration: '10s', target: 0 },   // Ramp down to 0 users over 30 seconds
+        { duration: '30s', target: 100 }, 
+        { duration: '50s', target: 100 }, 
+        { duration: '30s', target: 0 }, 
     ],
     thresholds: {
-        http_req_duration: ['p(99)<15000'], // 99% of requests must complete below 15 seconds
+        http_req_duration: ['p(99)<15000'], // 99% of requests must complete below 15s
     },
 };
 
 export default function () {
-    http.get('http://192.168.87.167:3000');
+    let res = http.get('http://192.168.87.167:3000');
+    check(res, { 'status was 200': (r) => r.status == 200 });
+    sleep(1);
 }
