a598098ef85d6d434faa8ddba4bc219f4c51d926
Swaiidan <yonswa01@easv365.dk>
2024-06-16 02:07:21 +0200

    Nyt

--- load_test.js
+++ load_test.js
@@ -7,7 +7,7 @@ export let options = {
         { duration: '30s', target: 0 }, 
     ],
     thresholds: {
-        http_req_duration: ['p(99)<1500'], // 99% of requests must complete below 1.5s
+        http_req_duration: ['p(99)<15000'], // 99% of requests must complete below 15s
     },
 };
 
